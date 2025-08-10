48030f417bb738063fb1714fdb4a6a76bd2d4a8e
鱼梦江湖 <zyyypx@qq.com>
2025-08-10 14:51:49 +0800

    Revert "Google Analytics"
    This reverts commit e0e0a19b9821502b279ac6118966fe1c52816b54.

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -23,9 +23,6 @@ import "vitepress-markdown-timeline/dist/theme/index.css";
 import { setupAutoAnchorOnScroll, internationalization } from './functions'
 import './custom.css'
 
-// Google Analytics
-import googleAnalytics from 'vitepress-plugin-google-analytics'
-
 // 工具函数导入
 import { useRoute, useData } from 'vitepress';
 import { onMounted, watch, nextTick, onUnmounted } from 'vue';
@@ -40,9 +37,6 @@ export default {
     ...DefaultTheme,
     Layout: NewLayout,
     enhanceApp(ctx) {
-        googleAnalytics({
-            id: 'G-RRQEZJHJ20' // 替换为您的 GoogleAnalytics ID，该 ID 应以“G-”开头
-        })
         // vitepressNprogress(ctx)
         ctx.app.component('Tags', Tags)
         ctx.app.component('Page', Page)
@@ -85,7 +79,7 @@ export default {
             if (observer) {
                 observer.disconnect();
             }
-
+            
             observer = new MutationObserver((mutations) => {
                 let shouldTrack = false;
                 mutations.forEach((mutation) => {
@@ -100,7 +94,7 @@ export default {
                         });
                     }
                 });
-
+                
                 if (shouldTrack) {
                     // 延迟执行以确保所有内容都已渲染
                     setTimeout(() => {
@@ -108,7 +102,7 @@ export default {
                     }, 50);
                 }
             });
-
+            
             observer.observe(document.body, {
                 childList: true,
                 subtree: true
